022a4bc656b52d68f10278ee450f4700a3d31ec8
cypher.zhong <1992zwm@gmail.com>
2019-05-27 17:57:57 +0800

    analyse form body

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer();
 
 /* GET users listing. */
-router.all('/:integrationId', function (req, res, next) {
+router.all('/:integrationId', upload.none(), function (req, res, next) {
     console.info(JSON.stringify(req.headers, null, 2), '\n ');
     console.info(JSON.stringify(req.params.integrationId, null, 2), '\n ');
     console.info(JSON.stringify(req.body));
