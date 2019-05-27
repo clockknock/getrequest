29807e98017d89f318c4f06f602637af03f5fce7
cypher.zhong <1992zwm@gmail.com>
2019-05-27 17:20:38 +0800

    add integrationId to url params

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.all('/', function (req, res, next) {
+router.all('/:integrationId', function (req, res, next) {
     console.info(JSON.stringify(req.headers, null, 2), '\n ');
+    console.info(JSON.stringify(req.params.integrationId, null, 2), '\n ');
     console.info(JSON.stringify(req.url, null, 2), '\n ');
     console.info(req.body);
     res.send('respond with a resource,hello');
