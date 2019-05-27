eaeebc91ca5bb88fc500550c4cda5fb115a8ad11
cypher.zhong <1992zwm@gmail.com>
2019-05-27 17:45:58 +0800

    try to get post data from beds24

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/:integrationId', function (req, res, next) {
+router.all('/:integrationId', function (req, res, next) {
     console.info(JSON.stringify(req.headers, null, 2), '\n ');
     console.info(JSON.stringify(req.params.integrationId, null, 2), '\n ');
-    console.info(JSON.stringify(req.url, null, 2), '\n ');
-    console.info(req.body);
-    res.statusCode = 403;
+    console.info(JSON.stringify(req.body));
     res.send('respond with a resource,hello');
 });
 
